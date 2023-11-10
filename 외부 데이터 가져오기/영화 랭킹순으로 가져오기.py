7c04b6d1b0a91b431bce7d697292665ea242e8f0
jeong in seo <128437656+GEUMAIN@users.noreply.github.com>
2023-11-10 14:34:31 +0900

    Create 영화 랭킹순으로 가져오기.py

---
+++ 외부 데이터 가져오기/영화 랭킹순으로 가져오기.py
@@ -0,0 +1,23 @@
+import requests
+
+def getRank(targetDt):
+    url = ('https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt='+targetDt)
+    #어떻게 웹에 접속하지?
+    response = requests.get(url)
+    if response.status_code == 200:
+        try :#시도해보고 잘 되면 이거 하고
+            # 결과(json)을 어떻게(dictionary) 바꿔야지?
+            mov = response.json()
+            # return 결과리스트
+            return mov['boxOfficeResult']['dailyBoxOfficeList']
+        except: #try에서 오류가 발생하면 이쪽으로
+            return []
+    else :
+        return[]
+dt = input('날짜를 적어주세요 (예 : yyyymmdd) : ')
+movies = getRank(dt) #제대로된 10개 리스트 또는 빈 리스트
+if len(movies) == 0:
+    print('값이 없습니다')
+else :
+    for movie in movies: #movies 10개의 영화 정보
+        print(movie['movieNm'])
