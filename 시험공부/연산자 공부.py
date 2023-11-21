46043cfdcb5268c5a95959e3e4ef63edb67a7db4
jeong in seo <128437656+GEUMAIN@users.noreply.github.com>
2023-11-21 09:36:58 +0900

    Create 연산자 공부.py

---
+++ 시험공부/연산자 공부.py
@@ -0,0 +1,17 @@
+str = 'hello'
+print(len(str))
+n = -13
+print(abs(n)) #abs = 절댓값
+
+p = pow(3,2) #pow = 제곱 (넣을 값,얼마나 제곱할 지)
+print(p)
+
+o = ord('A') #ord = 아스키코드(숫자) 값으로 변환
+print(o)
+
+c = chr(65) #chr 코드값(숫자)을 문자로 변환
+print(c)
+
+s = 'K'
+small_s = ord(s) + 32
+print(chr(small_s))
